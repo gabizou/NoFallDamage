40f5fc73c7ac89a6daf4df1db79530e3909b3114
Gabriel Harris-Rouquette <gabizou@me.com>
2017-07-13 09:36:53 -0700

    Add permission check with the damage event listener.
    Signed-off-by: Gabriel Harris-Rouquette <gabizou@me.com>

--- src/main/java/com/gabizou/NoFallDamage.java
+++ src/main/java/com/gabizou/NoFallDamage.java
@@ -1,23 +1,49 @@
+/*
+ * This file is part of NoFallDamage, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) Gabriel Harris-Rouquette <https://gabizou.com/>
+ * Copyright (c) contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package com.gabizou;
 
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.cause.entity.damage.DamageTypes;
 import org.spongepowered.api.event.cause.entity.damage.source.DamageSource;
 import org.spongepowered.api.event.entity.DamageEntityEvent;
+import org.spongepowered.api.event.filter.Getter;
 import org.spongepowered.api.event.filter.cause.Root;
 import org.spongepowered.api.plugin.Plugin;
+import org.spongepowered.api.service.permission.Subject;
 
 @Plugin(id = "nofalldamage",
         name = "NoFallDamage",
         authors = "gabizou",
-        version = "1.0.0",
+        version = "1.1.0",
         description = "Prevents players from taking fall damage"
 )
 public class NoFallDamage {
 
     @Listener
-    public void onFall(DamageEntityEvent event, @Root DamageSource source) {
-        event.setCancelled(source.getType().equals(DamageTypes.FALL));
+    public void onFall(DamageEntityEvent event, @Root DamageSource source, @Getter("getTargetEntity") Subject subject) {
+        event.setCancelled(source.getType().equals(DamageTypes.FALL) && subject.hasPermission("nofalldamage.takenodamage"));
     }
 
 }
